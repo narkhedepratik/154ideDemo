ddfb32bc7522b2cc2f18beda3abd7a15e21830cd
narkhedepratik <pratiknarkhede039@gmail.com>
2023-07-10 14:46:20 +0530

    anding enquiry module

--- DemoProject/src/main/java/com/bitlogic/main/DemoProjectApplication.java
+++ DemoProject/src/main/java/com/bitlogic/main/DemoProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("main class");
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
